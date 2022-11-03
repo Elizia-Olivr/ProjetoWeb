2c9e6f4b22d8d721f315b7b4810028d9307a0f82
Elízia Regina de Oliveira Silva <elizia.regina@academico.ifrn.edu.br>
2022-11-03 10:31:08 -0300

    Adicionar arquivos de projeto.

---
+++ Modelo/Tabelas/Categoria.cs
@@ -0,0 +1,16 @@
+﻿using Modelo.Cadastro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Modelo.Tabela
+{
+    public class Categoria
+    {
+        public long CategoriaId { get; set; }
+        public string Nome { get; set; }
+
+        public virtual ICollection<Produto> Produtos { get; set; }
+    }
+}
